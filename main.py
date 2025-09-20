0d9fb5912b1112dabc15d9fd6594f27cd8ab715a
craafty <the.elijah.sabin@gmail.com>
2025-09-20 13:39:02 -0400

    implement pilow

--- main.py
+++ main.py
@@ -1,5 +1,17 @@
 import tkinter as tk
+from PIL import Image
 
+img = Image.open("example.jpg")
+r, g, b = img.split()  # Split into individual channels
+
+# Reduce red by 50%
+g = g.point(lambda i: i * 0.0)
+
+# Recombine channels
+new_img = Image.merge("RGB", (r, g, b))
+new_img.save("reduced_red.jpg")
+
+"""
 root = tk.Tk()
 root.title("window title")
 root.geometry("400x300")
@@ -8,4 +20,4 @@ label = tk.Label(root, text="Hello World", font=("Arial", 16))
 label.pack(pady=20)
 
 root.mainloop()
-
+"""
