578abe28ac5a3399bb9ebe4fff6e8c43d8d10784
johanjosph <johantojo2007@gmail.com>
2025-09-20 17:55:50 -0400

    rotate_image function

--- imageFunctions.py
+++ imageFunctions.py
@@ -1,4 +1,22 @@
 from PIL import Image
-def main():
-    return("hello world")
+##def main():
+    ##return("hello world")
+
+
+    def rotate_image(input_path, output_path, degrees=90):
+    # Open the image
+    img = Image.open(input_path)
+
+    # Rotate the image
+    rotated_img = img.rotate(degrees, expand=True)
+
+    # Save the new image
+    rotated_img.save(output_path)
+
+    print(f"Rotated image saved to {output_path}")
+
+   
+
+
+
 
